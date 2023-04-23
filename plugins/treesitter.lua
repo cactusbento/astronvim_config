04cd9af65a31a7fb6261b51ea36ddbbfbb374c4f
CactusBento <cactusbento@outlook.com>
2023-04-23 13:52:15 -0400

    Init Commit for Custom

--- plugins/treesitter.lua
+++ plugins/treesitter.lua
@@ -1,6 +1,9 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   opts = {
+    highlight = { enable = true },
+    indent = { enabled = true },
+    auto_install = true,
     -- ensure_installed = { "lua" },
   },
 }
--- plugins/user.lua
+++ plugins/user.lua
@@ -2,11 +2,19 @@ return {
   -- You can also add new plugins here as well:
   -- Add plugins, the lazy syntax
   -- "andweeb/presence.nvim",
-  -- {
-  --   "ray-x/lsp_signature.nvim",
-  --   event = "BufRead",
-  --   config = function()
-  --     require("lsp_signature").setup()
-  --   end,
-  -- },
+  {
+    "ray-x/lsp_signature.nvim",
+    event = "BufRead",
+    config = function()
+      require("lsp_signature").setup({
+          hint_enable = true,
+          hint_prefix = "!>",
+      })
+    end,
+  },
+  {
+    "ishan9299/nvim-solarized-lua",
+    as = "solarized",
+    lazy = false,
+  },
 }
