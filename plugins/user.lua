8da4952e9857c7c6390b49b4c284038ebc023e30
cactusbento <cactusbento@outlook.com>
2023-05-10 12:44:12 -0400

    Added gopher.nvim

--- plugins/user.lua
+++ plugins/user.lua
@@ -1,21 +1,39 @@
 return {
-  -- You can also add new plugins here as well:
-  -- Add plugins, the lazy syntax
-  -- "andweeb/presence.nvim",
-  {
-    "ray-x/lsp_signature.nvim",
-    event = "BufRead",
-    config = function()
-      require("lsp_signature").setup({
-          hint_enable = true,
-          hint_prefix = "!>",
-		  toggle_key = '<M-x>',
-      })
-    end,
-  },
-  {
-    "ishan9299/nvim-solarized-lua",
-    as = "solarized",
-    lazy = false,
-  },
+	-- You can also add new plugins here as well:
+	-- Add plugins, the lazy syntax
+	-- "andweeb/presence.nvim",
+	{
+		"ray-x/lsp_signature.nvim",
+		event = "BufRead",
+		config = function()
+			require("lsp_signature").setup({
+					hint_enable = true,
+					hint_prefix = "!>",
+			toggle_key = '<M-x>',
+			})
+		end,
+	},
+	{
+		"ishan9299/nvim-solarized-lua",
+		as = "solarized",
+		lazy = false,
+	},
+	{
+		"olexsmir/gopher.nvim",
+		dependencies = { -- dependencies
+			"nvim-lua/plenary.nvim",
+		},
+		event = "BufEnter *.go",
+		config = function()
+			require("gopher").setup({
+				commands = {
+					go = "go",
+					gomodifytags = "gomodifytags",
+					gotests = "~/go/bin/gotests", -- also you can set custom command path
+					impl = "impl",
+					iferr = "iferr",
+				},
+			})
+		end
+	}
 }
